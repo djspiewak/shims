d5f907ee6d8da962bb59d84a6fb5b46e90e95d83
Daniel Spiewak <djspiewak@gmail.com>
2016-01-08 10:28:09 -0800

    Fleshed out implicit machinery a bit more

--- cats/src/main/scala/shims/Implicits.scala
+++ cats/src/main/scala/shims/Implicits.scala
@@ -1,5 +1,24 @@
+// cats
 package shims
 
-trait Implicits {
-  // TODO
+private[shims] trait LowPriorityImplicits3 {
+
+  implicit def functor1[F[_]](implicit F: _root_.cats.Functor[F]): shims.Functor[F] = ???
+}
+
+private[shims] trait LowPriorityImplicits2 extends LowPriorityImplicits3 {
+
+  implicit def applicative1[F[_]](implicit F: _root_.cats.Applicative[F]): shims.Applicative[F] = ???
+
+  implicit def flatMap1[F[_]](implicit F: _root_.cats.FlatMap[F]): shims.FlatMap[F] = ???
+}
+
+private[shims] trait LowPriorityImplicits1 extends LowPriorityImplicits2 {
+
+  implicit def monad1[F[_]](implicit F: _root_.cats.Monad[F]): shims.Monad[F] = ???
+}
+
+trait Implicits extends LowPriorityImplicits1 {
+
+  implicit def traverse1[F[_]](implicit F: _root_.cats.Traverse[F]): shims.Traverse[F] = ???
 }
--- scalaz/src/main/scala/shims/Implicits.scala
+++ scalaz/src/main/scala/shims/Implicits.scala
@@ -1,5 +1,24 @@
+// scalaz
 package shims
 
-trait Implicits {
-  // TODO
+private[shims] trait LowPriorityImplicits3 {
+
+  implicit def functor1[F[_]](implicit F: _root_.scalaz.Functor[F]): shims.Functor[F] = ???
+}
+
+private[shims] trait LowPriorityImplicits2 extends LowPriorityImplicits3 {
+
+  implicit def applicative1[F[_]](implicit F: _root_.scalaz.Applicative[F]): shims.Applicative[F] = ???
+
+  implicit def flatMap1[F[_]](implicit F: _root_.scalaz.Bind[F]): shims.FlatMap[F] = ???
+}
+
+private[shims] trait LowPriorityImplicits1 extends LowPriorityImplicits2 {
+
+  implicit def monad1[F[_]](implicit F: _root_.scalaz.Monad[F]): shims.Monad[F] = ???
+}
+
+trait Implicits extends LowPriorityImplicits1 {
+
+  implicit def traverse1[F[_]](implicit F: _root_.scalaz.Traverse[F]): shims.Traverse[F] = ???
 }
